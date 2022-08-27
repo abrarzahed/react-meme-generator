215ef0a3705d70ee69a6130a785532d4de43d215
abrarzahed <abrarzahed6986@gmail.com>
2022-08-28 00:12:55 +0600

    removed local meme data and and connected to meme API

--- components/MemeForm.jsx
+++ components/MemeForm.jsx
@@ -1,34 +1,58 @@
-import { useState } from "react";
-import memesData from "../memesData";
+import { useEffect, useState } from "react";
 
 export default function MemeForm() {
-  const memesArray = memesData.data.memes;
-
   const [meme, setMeme] = useState({
     topText: "",
     bottomText: "",
     randomImage: "http://i.imgflip.com/1bij.jpg",
   });
 
-  const [allMemeImages, setAllMemeImages] = useState(memesArray);
+  const [allMemes, setAllMemes] = useState([]);
+
+  useEffect(() => {
+    fetch("https://api.imgflip.com/get_memes")
+      .then((res) => res.json())
+      .then((memesRes) => {
+        setAllMemes(() => [...memesRes.data.memes]);
+      });
+  }, []);
 
   const getMemeImage = function (e) {
     e.preventDefault();
-
-    const randomMeme = Math.floor(Math.random() * allMemeImages.length);
-
-    setMeme((prev) => ({
-      ...prev,
-      randomImage: allMemeImages[randomMeme].url,
+    const randomMeme = Math.floor(Math.random() * allMemes.length);
+    const url = allMemes[randomMeme].url;
+    setMeme(() => ({
+      topText: "",
+      bottomText: "",
+      randomImage: url,
     }));
   };
+
+  function updateMemeText(event) {
+    const { name, value } = event.target;
+    setMeme((prev) => ({ ...prev, [name]: value }));
+  }
 
   return (
     <main className="app-content">
       <form>
         <div className="inputs">
-          <input type="text" id="setup" placeholder="Setup" />
-          <input type="text" id="punchline" placeholder="Punchline" />
+          <input
+            type="text"
+            id="setup"
+            placeholder="Setup"
+            name="topText"
+            value={meme.topText}
+            onChange={updateMemeText}
+          />
+          <input
+            type="text"
+            id="punchline"
+            placeholder="Punchline"
+            name="bottomText"
+            value={meme.bottomText}
+            onChange={updateMemeText}
+          />
         </div>
 
         <div className="actions">
@@ -41,7 +65,9 @@ export default function MemeForm() {
       </form>
 
       <div className="meme">
+        <h1 className="setup">{meme.topText}</h1>
         <img src={meme.randomImage} alt="" />
+        <h1 className="punchline">{meme.bottomText}</h1>
       </div>
     </main>
   );
